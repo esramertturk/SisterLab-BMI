7e611bb0d1da4fb52ddaa0bcc18755ae323fb4ae
esramertturk <37470761+esramertturk@users.noreply.github.com>
2020-08-22 16:03:05 +0300

    Update LoginActivity.kt
    Açıklama satırları eklendi.

--- app/src/main/java/com/example/sisterlab_bmi/LoginActivity.kt
+++ app/src/main/java/com/example/sisterlab_bmi/LoginActivity.kt
@@ -23,19 +23,38 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun initialize() {
+        //aşağıdaki tanımlamaları yapmamız şart değil, kotlin tanımlama yapmadan da kolaylık sağlıyor 
         var etUserName = findViewById(R.id.et_user_name) as EditText
         var etPassword = findViewById(R.id.et_password) as EditText
         var btnLogin = findViewById(R.id.btn_login) as Button
 
+        //butonun tıklanabilme özelliği
         btnLogin.setOnClickListener {
+            //if yapısı: eğer koşul gerçeklendiğinde ne olacak, gerçekleşmediğinde ne olacak? sorularına karşılık veren yapıdır.
+            //isempty() ona verilen değerin boş olup olmadığını kontrol eder.
+            //seterror: satırda hata mesajı vermeye yarar.
+            
+            //kulanıcı adını boş olup olmamasına baktık, eğer boş ise hata mesajı çıkacaktır.
             if (isEmpty(etUserName.text)) {
                 etUserName.setError("Kullanıcı adı boş geçilemez!");
             }
+            
+            //şifrenin boş olup olmamasına baktık, eğer boş ise hata mesajı çıkacaktır.
             if (isEmpty(etPassword.text)) {
                 etPassword.setError("Şife boş geçilemez!");
             }
+            
+            //contains: text ifadeler üzerinde karşılaştırma yapılacak ise kullanlır. 
+            //burada kullanıcının girdiği değer ile en yukarıda global olarak tanımlanan userName ve password değişkenini karşılatırıyoruz.
+            //Sonuç true(doğru) ise: sayfa geçişi olacak, sonuç false (yanlış) ise toast mesajı ile ekrana hata basılacaktır.
            if (etUserName.text.contains(userName) && etPassword.text.contains(password))
             {
+                //sayfalar arası geçişlerde ve veri taşımalarda INTENT kullanılır.
+                //gecis değişkenine intenti tanımlıyoruz ve hangi activity ye gideceğini belirliyoruz
+                //putextra veri taşımada bize yardımcı olacak mettodur. Key,value şeklinde değer alır. 
+                // Key: anahtar görevi görür ve taşınacak verinin diğer sayfada kullanabilmeyi sağlar.
+                // Value: anahtar ile hangi değerin taşınacağını belirtir.
+                //StartActivity ise intentte bahsedilen sayfanın açılmasını sağlar.
                 val gecis = Intent(this,MainActivity::class.java)
                 gecis.putExtra("UserName", etUserName.text.toString())
                 startActivity(gecis)
